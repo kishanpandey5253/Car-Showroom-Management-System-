4867e66b0546473a96490254e34392d564855a7a
kishan pandey <kishanpandey5253@gmail.com>
2026-08-23 22:26:41 +0530

    Time: 4 ms (74.65%) | Memory: 45.4 MB (63.88%) - LeetSync

---
+++ 4381-check-ascii-palindromic/check-ascii-palindromic.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isPalindromic(String s) {
+
+        StringBuilder binary = new StringBuilder();
+
+        for (char c:s.toCharArray()){
+            int ascii = (int) c;
+
+            String bits = Integer.toBinaryString(ascii);
+
+            while(bits.length() < 8){
+                bits = "0" + bits;
+            }
+
+             binary.append(bits);
+        }
+
+        int left = 0;
+        int right = binary.length()-1;
+
+
+        while (left<right) {
+            if(binary.charAt(left)!=binary.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        //FOR FINAL ANSWER
+        return true;
+    }
+}
